e9e3c9fd5f25ed3005aa96ff9f25888eb11b3964
jegathish jega <jegathish.vk18@gmail.com>
2025-09-10 13:26:24 +0530

    your message

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       themeMode: ThemeMode.system,
       home: SplashScreen(),
     );
